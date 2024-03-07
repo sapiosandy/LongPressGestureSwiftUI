152d85efad5de1775e0661fcac493114385f5a88
Sandy Gomez <sgomez1029@gmail.com>
2024-03-06 18:30:50 -0800

    Initial Commit

---
+++ LongPressGestureSwiftUI/LongPressGestureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressGestureSwiftUIApp.swift
+//  LongPressGestureSwiftUI
+//
+//  Created by Sandra Gomez on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LongPressGestureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
