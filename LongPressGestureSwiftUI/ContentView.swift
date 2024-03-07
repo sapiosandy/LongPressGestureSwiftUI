8c6add4705f47d50ee5a9c8b92f74ee12f6364bd
Sandy Gomez <sgomez1029@gmail.com>
2024-03-06 18:45:44 -0800

    Long Press Gesture in SwiftUI (UCSD Extension)

--- LongPressGestureSwiftUI/ContentView.swift
+++ LongPressGestureSwiftUI/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var flag = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+            Image(systemName: "star.fill")
+                .font(.system(size: 200))
+                .foregroundColor(flag ? .green : .red)
+                .gesture(
+                    LongPressGesture()
+                    .onEnded({ _ in flag.toggle()
+                        if flag {
+                            message = "Green star"
+                        } else {
+                            message = "red star"
+                            }
+                        })
+                )
         }
         .padding()
     }
